7418ba39274625136f1812ec9bb776ea8202dfdc
Radomir Skórka <147660435+r-skorka@users.noreply.github.com>
2025-01-23 08:56:32 +0100

    Update main.py

--- ZADANIE5/main.py
+++ ZADANIE5/main.py
@@ -15,25 +15,33 @@ def create_app():
     3) Przycisk "Pokaż", który wyświetli wpisany tekst w innej etykiecie
     """
     # title - "Prosta aplikacja Tkinter"
-
+    root = tk.Tk()
+    root.geometry("300x300")
+    root.title("Prosta aplikacja Tkinter")
+    
     # label_instruct = umocuj przez pack
-
+    label_instruct = tk.Label(root, text="Wpisz coś:")
+    label_instruct.pack()
+    
     # entry_text = 
+    entry_text = tk.Entry(root)
+    entry_text.pack()
 
     # label_result = tk.Label(...
+    label_result = tk.Label(root, text="Tu pojawi się Twój tekst")
+    label_result.pack()
 
     # zdefiniuj funkcję show_text() pobierającą wpisany tekst i wyświetlającą w label_result
-    # def show_text():
-    #     ...
-    #     label_result.config(...)
+    def show_text():
+        text = entry_text.get()
+        label_result.config(text=f"Wpisałeś: {text}")
 
-    # button_show = 
-    # button_show.pack()
+    button_show = tk.Button(root, text="Pokaż", command=show_text)
+    button_show.pack()
 
     # TODO: return root
-    pass
+    return root
 
 if __name__ == '__main__':
-    # TODO: app = create_app()
-    # TODO: app.mainloop()
-    pass
+    app = create_app()
+    app.mainloop()
